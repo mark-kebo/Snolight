497c66e63af67d722c9f7eee1d917a7fd2f6933b
Mark Kebo <arolij.corp@gmail.com>
2018-09-11 22:02:31 +0300

    small fix

--- app/src/main/java/com/vorozhbicky/dmitry/snolight/ChartActivity.java
+++ app/src/main/java/com/vorozhbicky/dmitry/snolight/ChartActivity.java
@@ -8,9 +8,11 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -73,10 +75,6 @@ public class ChartActivity extends AppCompatActivity {
                 nameChart = getString(R.string.app_name);
         }
         super.setTitle(nameChart);
-        initRecyclerView();
-    }
-
-    private void initRecyclerView() {
         readingsListView = findViewById(R.id.readings_list_view);
     }
 
